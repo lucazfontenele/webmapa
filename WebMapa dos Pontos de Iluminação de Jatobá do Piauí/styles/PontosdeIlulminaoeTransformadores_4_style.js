e2795c89b9ada965c80b3d5d4cdb7858a616bb84
lucazfontenele <78514636+lucazfontenele@users.noreply.github.com>
2021-02-03 21:43:04 -0300

    Add files via upload

---
+++ WebMapa dos Pontos de Iluminação de Jatobá do Piauí/styles/PontosdeIlulminaoeTransformadores_4_style.js
@@ -0,0 +1,32 @@
+var size = 0;
+var placement = 'point';
+
+var style_PontosdeIlulminaoeTransformadores_4 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = ""
+    var labelText = "";
+    size = 0;
+    var labelFont = "7.800000000000001px \'Century Gothic\', sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if (feature.get("ID") !== null) {
+        labelText = String(feature.get("ID"));
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 4.0 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(225,249,44,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
